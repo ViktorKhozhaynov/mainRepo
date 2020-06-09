792067535242497e9ef0d03c40d06f75f7fa36b3
Viktor <vithozh410@gmail.com>
2020-06-09 11:54:44 +0300

    New implementation of the framework
    - docker compose process finilized

--- Python2/config.py
+++ Python2/config.py
@@ -6,5 +6,5 @@ default_page_load_timeout = 20
 
 base_url = 'http://automationpractice.com/index.php'
 
-remote_driver_host = os.getenv('SELENIUM_HUB_HOST', 'X.X.X.X')
+remote_driver_host = os.getenv('SELENIUM_HUB_HOST', 'selenium_hub')
 remote_port = 4444
